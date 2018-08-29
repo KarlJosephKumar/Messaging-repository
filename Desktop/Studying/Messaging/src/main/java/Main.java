88c322e2f19b6a5b6f2eb77740e999a412bbf0c8
KarlJosephKumar <kjkumr@gmail.com>
2018-08-29 21:14:56 +0300

    Third commit; Finished main Made add and login methods to FileUserService

--- Desktop/Studying/Messaging/src/main/java/Main.java
+++ Desktop/Studying/Messaging/src/main/java/Main.java
@@ -3,28 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        FileUserService fileUserService = new FileUserService();
         while (true) {
             System.out.println("1. Sign up");
             System.out.println("2. Log in");
             int answer = Integer.parseInt(scanner.nextLine());
             switch (answer) {
                 case 1:
-                    System.out.println("Enter email: ");
-                    String email = scanner.nextLine();
-                    System.out.println("Enter your password");
-                    String password = scanner.nextLine();
-                    System.out.println("Enter your full name: ");
-                    String name = scanner.nextLine();
-                    System.out.println("Enter your age: ");
-                    int age = scanner.nextInt();
-                    User user = new User(email, name, age, password);
+                    fileUserService.addUser();
+
                 case 2:
-                    System.out.println("Enter your email: ");
-                    String enteredEmail = scanner.nextLine();
-                    System.out.println("Enter password: ");
-                    String enteredPassword = scanner.nextLine();
-                    if(enteredEmail == user.getEmail() && enteredPassword == user.getPassword()){
-                        //login
+                    fileUserService.login();
                     }
 
 
