c92451ae6f83b5ab59171fece085e265ce5ad7b2
Sobolvitya <vasobol1995@gmail.com>
2018-08-31 18:53:59 +0300

    Revert "update"
    This reverts commit 3b4f5d3ca641c9ce4e9e139d331b26449c7a9cf2.

--- Desktop/Studying/Messaging/src/main/java/Main.java
+++ Desktop/Studying/Messaging/src/main/java/Main.java
@@ -3,23 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        FileUserService fileUserService = new FileUserService(new IOUtils());
+        FileUserService fileUserService = new FileUserService();
         while (true) {
             System.out.println("1. Sign up");
             System.out.println("2. Log in");
-            System.out.println("0. Exit");
             int answer = Integer.parseInt(scanner.nextLine());
             switch (answer) {
                 case 1:
                     fileUserService.addUser();
-                    break;
-                case 2:
-                    //fileUserService.login();
-                    break;
-                case 0:
-                    return;
 
-            }
+                case 2:
+                    fileUserService.login();
+                    }
+
+
+
         }
     }
 }
